4dedd23f72cbede91d7c3134c49af912f35d8f99
Quoc Thinh <eeit104@gmail.com>
2023-08-28 11:10:15 +0700

    Generate token by chatbot

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 import dotenv from 'dotenv';
 import TelegramBot from 'node-telegram-bot-api';
-import gptService from "./services/gpt.service.js";
-import databaseService from "./services/database.service.js";
+import admin from 'firebase-admin';
 
 dotenv.config();
 
 const telegramToken = process.env.TELEGRAM_BOT_KEY;
+const appCheckID = process.env.APP_CHECK_ID
 
 const bot = new TelegramBot(telegramToken, {polling: true});
-const welcomeMsg = (name = 'bạn') => `Chào mừng ${name}, tôi là bot của Nanxy, hãy hỏi tôi bằng tiếng anh, vì Nanxy không có đủ $ để trả tiền cho davinci model (loại model giúp tôi trả lời trong nhiều thứ tiếng)`
 
-databaseService.connect().then(() => {
-    bot.on('message', async (msg) => {
-        const chatId = msg.chat.id;
-        const chatMsg = msg.text;
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const chatMsg = msg.text;
 
-        const chatAuthor = await databaseService.getUserByTelegramId(msg.from.id, `${msg.from.first_name} ${msg.from.last_name}`)
+    admin.initializeApp({
+        credential: admin.credential.cert("./credential/service_account.json")
+    })
 
-        if (msg.entities?.[0]?.type === 'bot_command') {
-            return await bot.sendMessage(chatId, welcomeMsg(chatAuthor.username))
-        }
-
-        const responseMsg = await gptService.generateCompletion(chatMsg)
-        if (responseMsg) {
-            await Promise.all([
-                databaseService.createNewMessage(chatAuthor, chatMsg, responseMsg),
-                bot.sendMessage(chatId, responseMsg)
-            ])
-        }
-
-    });
-})
+    if (chatMsg === 'please-token') {
+        const token = await admin.appCheck().createToken(appCheckID, {
+            ttlMillis: 3600000,
+        });
+        return bot.sendMessage(chatId, token.token)
+    }
+});
 
